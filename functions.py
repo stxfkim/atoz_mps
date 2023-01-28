dc8a711f12e1b8ef9e8f65599780556c1c4bcf94
stxfkim <stefkim@outlook.co.id>
2023-01-28 19:15:52 +0700

    keyError 1 bug fixed

--- functions.py
+++ functions.py
@@ -38,37 +38,37 @@ def calculate_salary(row):
 
 def generate_kwitansi(row):
     file_list = []
-    for idx in row.index:
+    for idx,row in row.iterrows():
         wb = load_workbook("Template Kwitansi.xlsx")
         sheet = wb.active
 
         #reading specific column
         # B3 - Nama
-        sheet.cell(row=3, column=2).value=row["Nama"][idx]
+        sheet.cell(row=3, column=2).value=row["Nama"]
         sheet.merge_cells('B3:I6')
         sheet.cell(row=3, column=2).alignment = Alignment(horizontal='center', vertical='center')
         # H10 & E24 - gaji_final
-        sheet.cell(row=10, column=8).value=row["gaji_final"][idx]
+        sheet.cell(row=10, column=8).value=row["gaji_final"]
         sheet.cell(row=10, column=8).alignment = Alignment(horizontal='center', vertical='center')
         sheet.merge_cells('H10:J10')  
-        sheet.cell(row=24, column=5).value=row["gaji_final"][idx]
+        sheet.cell(row=24, column=5).value=row["gaji_final"]
         sheet.merge_cells('E24:K24')
         sheet.cell(row=24, column=5).alignment = Alignment(horizontal='left', vertical='center')
         # G28 - Nama Bank & Nama Akun Bank
         sheet.cell(row=28, column=7).value=row["Nama Bank"][1] + " A/n "+ row["Nama Akun Bank"][1]
         # G30 - Nomor Rekening
-        sheet.cell(row=30, column=7).value=row["Nomor Rekening"][idx]
+        sheet.cell(row=30, column=7).value=row["Nomor Rekening"]
         # V5 & G32 - tanggal dicetak
         sheet.cell(row=5, column=22).value= date.today().strftime('%d %b %Y')
         sheet.cell(row=32, column=7).value= date.today().strftime('%d %b %Y')
         # V3 - Nomor Kwitansi
         sheet.cell(row=3, column=22).value= "KWT_ATS_VII-22_"
         # M14 & R14 - Periode Upah
-        sheet.cell(row=14, column=13).value=row["start_date"][idx]
-        sheet.cell(row=14, column=18).value=row["end_date"][idx]
-        wb.title = row["nama_worksheet"][idx]
-        sheet.title = row["nama_worksheet"][idx]
-        file_name = "Kwitansi_"+row["nama_worksheet"][idx]+"_"+str(row["start_date"][idx].strftime('%d%b'))+"-"+str(row["end_date"][idx].strftime('%d%b%Y'))+".xlsx"
+        sheet.cell(row=14, column=13).value=row["start_date"]
+        sheet.cell(row=14, column=18).value=row["end_date"]
+        wb.title = row["nama_worksheet"]
+        sheet.title = row["nama_worksheet"]
+        file_name = "Kwitansi_"+row["nama_worksheet"]+"_"+str(row["start_date"].strftime('%d%b'))+"-"+str(row["end_date"].strftime('%d%b%Y'))+".xlsx"
         wb.save("kwitansi_output/"+file_name)
         file_list.append("kwitansi_output/"+file_name)
     return file_list
